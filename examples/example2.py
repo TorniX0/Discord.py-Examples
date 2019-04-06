f41769df4c4445c8fe75f1307c3ee904f451098f
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-06 23:51:49 +0300

    Create example2.py

---
+++ examples/example2.py
@@ -0,0 +1,28 @@
+import discord
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print('OK >> BOT IS READY!')
+    print(discord.__version__)
+
+@client.command()
+async def embed(ctx):
+    embed = discord.Embed(colour=discord.Colour.yellow()) #this is the definition of the embed
+    embed.set_author(name="Hello, this is the author.") #this is the author
+    embed.add_field(name='Hey there!', value='This is a field', inline=True) #this is a field
+    embed.add_field(name='Howdy!', value='This is a field with inline=true', inline=True) #this is a inline=True field
+    embed.add_field(name='Ayy!', value='This is a field with inline=false', inline=False) #this is a inline=False field
+    embed.set_image(url='URL For a image!') #this is a set_image
+    embed.set_thumbnail(url='URL For a thumbnail!') #this is a thumbnail
+    embed.set_footer(text='This is a footer.') #this is the footer
+    await ctx.send(embed=embed) #here we are sending it to the channel
+
+@client.command()
+async def mention(ctx):
+    await ctx.send(f"{ctx.author.mention}, you have been pinged!") #ctx.author is basically short for ctx.message.author same for: ctx.guild, ctx.send, etc..
+
+bot.run("token") #insert token here
