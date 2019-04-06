893b76d2e415e8879c502e0ca023b4338d36a023
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-07 00:04:32 +0300

    Create example3.py

---
+++ examples/example3.py
@@ -0,0 +1,24 @@
+import discord
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print('OK >> BOT IS READY!')
+    print(discord.__version__)
+
+@client.command()
+async def dm_me(ctx):
+    await ctx.author.send("I've just dm'd you!") #sends a dm
+    await ctx.message.delete() #deletes the sent message by the user
+
+@client.command(alias=['permc, pc']) #aliases
+async def permissions_check(ctx):
+    if ctx.author.guild_permissions.administrator: #check if user has administrator
+        await ctx.send('You are allowed to proceed.')
+    else:
+        await ctx.send('You are denied to proceed.')
+
+bot.run("token")
