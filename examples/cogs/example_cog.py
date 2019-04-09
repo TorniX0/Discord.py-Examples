04bfa76382e64901bdd9177338ecb958eac06233
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-09 15:08:39 +0300

    Update example_cog.py

--- examples/cogs/example_cog.py
+++ examples/cogs/example_cog.py
@@ -6,6 +6,13 @@ class example_cog(commands.Cog): #it must inherit from commands.Cog
     def __init__(self, bot):
         self.bot = bot #so you're gonna use self.bot in cogs if you have it like this.
 
+    #listeners now must have a decorator
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "cookie" in message.content.lower():
+            await message.channel.send('But I want to keep them :(.. :cookie: :cookie: :cookie: :cookie: :cookie:')
+        await self.bot.process_commands(message)
+        
     @commands.command(aliases=['dtcw'])
     async def does_the_cog_work(self, ctx): #first 2 arguments must be self and ctx
         await ctx.send('The cog works! Everything works correctly and fine.')
