ddbab58eaa7aa656cb84a647ba0f8569fd9ff397
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-12 11:59:00 +0300

    Update example5.py

--- examples/example5.py
+++ examples/example5.py
@@ -1,4 +1,6 @@
-import discord, traceback
+import discord
+import traceback
+import os
 
 from discord.ext import commands
 
@@ -10,7 +12,7 @@ async def on_ready():
     print(discord.__version__)
 
 if __name__ == "__main__":
-    for extension in [f.replace('.py', '') for f in listdir("cogs") if isfile(join("cogs", f))]:
+    for extension in [f.replace('.py', '') for f in os.listdir("cogs") if os.isfile(join("cogs", f))]:
         try:
             bot.load_extension("cogs." + extension)
         except Exception as e:
