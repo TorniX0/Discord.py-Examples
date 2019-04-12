9d4e766afe0f8336c81abe8104afff60cb901f79
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-12 11:42:01 +0300

    Update example_cog2.py

--- examples/cogs/example_cog2.py
+++ examples/cogs/example_cog2.py
@@ -1,4 +1,5 @@
-import discord, asyncio #you could always use from asyncio import sleep :)
+import discord
+import asyncio   # you could always use from asyncio import sleep :)
 
 from discord.ext import commands
 
@@ -8,17 +9,9 @@ class example_cog2(commands.Cog):
         
     @commands.command(aliases=['cd', 'ctd'])
     async def countdown(self, ctx):
-        await ctx.send("5")
-        await asyncio.sleep(1) #seconds, and you can always use asyncio import sleep as specified above, but you'll have to use
-        #await sleep().
-        await ctx.send("4..")
-        await asyncio.sleep(1)
-        await ctx.send("3...")
-        await asyncio.sleep(1)
-        await ctx.send("2....")
-        await asyncio.sleep(1)
-        await ctx.send("1.....")
-        await asyncio.sleep(1)
+        for a in range(-5, 0):
+            await ctx.send(f"{abs(a)}...")
+            await asyncio.sleep(1)
         await ctx.send("**GO!**")
 
 def setup(bot):
