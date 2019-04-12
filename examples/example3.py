234e07a649bba3760ce00125436e39905d1854c2
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-12 11:32:34 +0300

    Update example3.py

--- examples/example3.py
+++ examples/example3.py
@@ -11,12 +11,12 @@ async def on_ready():
 
 @bot.command()
 async def dm_me(ctx):
-    await ctx.author.send("I've just dm'd you!") #sends a dm
-    await ctx.message.delete() #deletes the sent message by the user
+    await ctx.author.send("I've just dm'd you!")   # sends a dm
+    await ctx.message.delete()   # deletes the sent message by the user
 
-@bot.command(aliases=['permc', 'pc']) #aliases
+@bot.command(aliases=['permc', 'pc'])   # aliases
 async def permissions_check(ctx):
-    if ctx.author.guild_permissions.administrator: #check if user has administrator
+    if ctx.author.guild_permissions.administrator:   # check if user has administrator
         await ctx.send('You are allowed to proceed.')
     else:
         await ctx.send('You are denied to proceed.')
