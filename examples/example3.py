56310fffbb058ab02672e388810bd1f153c85de2
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2019-04-13 16:24:07 +0300

    Update example3.py

--- examples/example3.py
+++ examples/example3.py
@@ -11,12 +11,14 @@ async def on_ready():
 
 @bot.command()
 async def dm_me(ctx):
+    """This command will DM you."""
     await ctx.author.send("I've just dm'd you!")   # sends a dm
     await ctx.message.delete()   # deletes the sent message by the user
 
 @bot.command(aliases=['permc', 'pc'])   # aliases
 @bot.has_permissions(administrator=True)
 async def permissions_check(ctx):
+    """This command checks if you have Administrator permission in your role/s."""
     await ctx.send('You are allowed to proceed.')
 
 bot.run("token")
